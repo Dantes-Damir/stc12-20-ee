6fd96d9fe5e2cb1d85b619cf7c031767f2a2dcb5
MVBiryukov-stc11 <m.biryukov@innopolis.ru>
2018-10-01 21:14:28 +0300

    Initial commit

---
+++ MobilePhones_public_cities.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.cities (id, name, citizens) VALUES (2, 'Sankt-Peterburg', 8000000);
+INSERT INTO public.cities (id, name, citizens) VALUES (3, 'Kazan', 2000000);
+INSERT INTO public.cities (id, name, citizens) VALUES (4, 'Samara', 1000000);
+INSERT INTO public.cities (id, name, citizens) VALUES (1, 'Moskva', 15000000);
---
+++ MobilePhones_public_students.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.students (id, name, family_name, age, contact, city) VALUES (10, 'tets', 'test', 21, 'sese', 4);
+INSERT INTO public.students (id, name, family_name, age, contact, city) VALUES (11, 'tets', 'test', 21, 'sese', 4);
+INSERT INTO public.students (id, name, family_name, age, contact, city) VALUES (4, 'Petr', 'Ivanov', 20, 'No contact', 4);
+INSERT INTO public.students (id, name, family_name, age, contact, city) VALUES (2, 'Fedor', 'Smolov', 20, '+7 337 454 48 37', 2);
+INSERT INTO public.students (id, name, family_name, age, contact, city) VALUES (7, 'Vladimir', 'Egorov', 20, null, 3);
+INSERT INTO public.students (id, name, family_name, age, contact, city) VALUES (9, 'Alexander', 'Kudryavtsev', 34, 'dfdfdfd', 1);
+INSERT INTO public.students (id, name, family_name, age, contact, city) VALUES (5, 'Mikhail', 'Nekhoroshev', 24, 'sdfdfsdfsfsf@sdfsd.ru', 2);
